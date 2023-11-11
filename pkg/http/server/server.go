7f514d4725a50d340a17e43bd45392718cbade9a
niels <niels@prodsec.co>
2023-11-11 15:40:17 -0500

    Add more fields from the request struct

--- pkg/http/server/server.go
+++ pkg/http/server/server.go
@@ -7,6 +7,7 @@ import (
 	"greyhole/backend_service"
 	"greyhole/pkg/client"
 	"io"
+	"log"
 	"net/http"
 )
 
@@ -28,10 +29,55 @@ func (h *HttpServer) Start(listen_string string) error {
 }
 
 func (h *HttpServer) catchAll(w http.ResponseWriter, r *http.Request) {
-
 	pr := &backend_service.HandleProbeRequest{
-		Method:     r.Method,
 		RequestUri: r.RequestURI,
+		Request: &backend_service.HttpRequest{
+			Method:        r.Method,
+			Proto:         r.Proto,
+			ContentLength: r.ContentLength,
+			RemoteAddress: r.RemoteAddr,
+			ParsedUrl: &backend_service.ParsedURL{
+				Scheme:   r.URL.Scheme,
+				User:     r.URL.User.Username(),
+				Host:     r.URL.Host,
+				Port:     r.URL.Port(),
+				Path:     r.URL.Path,
+				RawPath:  r.URL.RawPath,
+				RawQuery: r.URL.RawQuery,
+				Fragment: r.URL.Fragment,
+			},
+		},
+	}
+
+	// Important to keep in mind that a parameter might be repeated with the same
+	// name but a different value.
+	for k, v := range r.URL.Query() {
+		pr.Request.ParsedUrl.Query = append(pr.Request.ParsedUrl.Query,
+			&backend_service.KeyValues{
+				Key:   k,
+				Value: v,
+			})
+	}
+	// Passwords are optional.
+	pass, isSet := r.URL.User.Password()
+	if isSet {
+		pr.Request.ParsedUrl.Password = pass
+	}
+
+	for k, v := range r.Header {
+		pr.Request.Header = append(pr.Request.Header, &backend_service.KeyValues{
+			Key:   k,
+			Value: v,
+		})
+	}
+
+	if r.Body != nil {
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Printf("parsing body: %s", err)
+		} else {
+			pr.Request.Body = string(b)
+		}
 	}
 
 	h.client.HandleProbeRequest(pr)
