3dbd4671b0ab7f80b1aa41f6e967b990695861cf
niels <niels@prodsec.co>
2023-11-24 12:54:15 -0500

    Add more tests for API server

--- pkg/api/server_test.go
+++ pkg/api/server_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"errors"
 	"io"
 	"loophid/pkg/database"
@@ -10,22 +11,100 @@ import (
 	"testing"
 )
 
-func getContentResponseForURL(queryStr string, content string, err error) *http.Response {
-	fd := database.FakeDatabaseClient{
-		ContentToReturn: database.Content{
-			Content: content,
+func TestUpsertSingleContent(t *testing.T) {
+	for _, test := range []struct {
+		description           string
+		queryString           string
+		status                string
+		statusMessageContains string
+		statusCode            int
+		err                   error
+	}{
+		{
+			description:           "Insert OK",
+			queryString:           "/?name=foo&content_type=text&server=Apache",
+			status:                "OK",
+			statusMessageContains: "Added",
+			statusCode:            200,
+			err:                   nil,
 		},
-		ErrorToReturn: err,
-	}
-	s := NewApiServer(&fd)
+		{
+			description:           "Updated OK",
+			queryString:           "/?id=2&name=foo&content_type=text&server=Apache",
+			status:                "OK",
+			statusMessageContains: "Updated",
+			statusCode:            200,
+			err:                   nil,
+		},
+		{
+			description:           "Insert fails with error",
+			queryString:           "/?name=foo&content_type=text&server=Apache",
+			statusMessageContains: "fail",
+			status:                "NOK",
+			statusCode:            200,
+			err:                   errors.New("fail"),
+		},
+		{
+			description:           "Update fails with error",
+			queryString:           "/?id=2&name=foo&content_type=text&server=Apache",
+			statusMessageContains: "fail",
+			status:                "NOK",
+			statusCode:            200,
+			err:                   errors.New("fail"),
+		},
+		{
+			description:           "Update fails on ID",
+			queryString:           "/?id=FAIL&name=foo&content_type=text&server=Apache",
+			statusMessageContains: "Unable to parse",
+			status:                "NOK",
+			statusCode:            200,
+			err:                   nil,
+		},
+		{
+			description:           "Missing parameters",
+			queryString:           "/?id=32",
+			statusMessageContains: "parameters given",
+			status:                "NOK",
+			statusCode:            200,
+			err:                   nil,
+		},
+	} {
 
-	req := httptest.NewRequest(http.MethodGet, queryStr, nil)
-	w := httptest.NewRecorder()
-	s.HandleGetSingleContent(w, req)
-	return w.Result()
+		t.Run(test.description, func(t *testing.T) {
+
+			fd := database.FakeDatabaseClient{
+				ErrorToReturn: test.err,
+			}
+			s := NewApiServer(&fd)
+
+			req := httptest.NewRequest(http.MethodGet, test.queryString, nil)
+			w := httptest.NewRecorder()
+			s.HandleUpsertSingleContent(w, req)
+			res := w.Result()
+
+			// Check the request body
+			defer res.Body.Close()
+			data, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Errorf("reading response body: %s", err)
+			}
+
+			pdata := HttpResult{}
+			if err = json.Unmarshal(data, &pdata); err != nil {
+				t.Errorf("error parsing response: %s (%s)", err, string(data))
+			}
+
+			if pdata.Status != test.status {
+				t.Errorf("status %s expected, got %s", test.status, pdata.Status)
+			}
+			if !strings.Contains(pdata.Message, test.statusMessageContains) {
+				t.Errorf("expected \"%s \"in status message %s", test.statusMessageContains, pdata.Message)
+			}
+		})
+	}
 }
 
-func TestGetSingleContentOK(t *testing.T) {
+func TestGetSingleContent(t *testing.T) {
 	for _, test := range []struct {
 		description   string
 		queryString   string
@@ -57,7 +136,20 @@ func TestGetSingleContentOK(t *testing.T) {
 	} {
 
 		t.Run(test.description, func(t *testing.T) {
-			res := getContentResponseForURL(test.queryString, test.contentString, test.err)
+
+			fd := database.FakeDatabaseClient{
+				ContentToReturn: database.Content{
+					Content: test.contentString,
+				},
+				ErrorToReturn: test.err,
+			}
+			s := NewApiServer(&fd)
+
+			req := httptest.NewRequest(http.MethodGet, test.queryString, nil)
+			w := httptest.NewRecorder()
+			s.HandleGetSingleContent(w, req)
+			res := w.Result()
+
 			// Check the request body
 			defer res.Body.Close()
 			data, err := io.ReadAll(res.Body)
@@ -71,6 +163,75 @@ func TestGetSingleContentOK(t *testing.T) {
 
 			if res.StatusCode != test.statusCode {
 				t.Errorf("status code %d expected, got %d", test.statusCode, res.StatusCode)
+			}
+		})
+	}
+}
+
+func TestGetSingleContentRule(t *testing.T) {
+	for _, test := range []struct {
+		description string
+		queryString string
+		path        string
+		contentId   int64
+		statusCode  int
+		err         error
+	}{
+		{
+			description: "fetch successful",
+			queryString: "/contentrule/get?id=42",
+			path:        "/this/path",
+			contentId:   42,
+			statusCode:  200,
+			err:         nil,
+		},
+		{
+			description: "fetch fails",
+			queryString: "/contentrule/get?id=42",
+			path:        "/this/path",
+			contentId:   42,
+			statusCode:  500,
+			err:         errors.New("fail fail"),
+		},
+
+		{
+			description: "id parsing fails",
+			queryString: "/contentrule/get?id=FAIL",
+			path:        "/this/path",
+			contentId:   42,
+			statusCode:  500,
+			err:         nil,
+		},
+	} {
+
+		t.Run(test.description, func(t *testing.T) {
+			fd := database.FakeDatabaseClient{
+				ContentRuleToReturn: database.ContentRule{
+					Path:      test.path,
+					ContentID: test.contentId,
+				},
+				ErrorToReturn: test.err,
+			}
+			s := NewApiServer(&fd)
+
+			req := httptest.NewRequest(http.MethodGet, test.queryString, nil)
+			w := httptest.NewRecorder()
+			s.HandleGetSingleContentRule(w, req)
+			res := w.Result()
+
+			// Check the request body
+			defer res.Body.Close()
+			data, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Errorf("reading response body: %s", err)
+			}
+
+			if res.StatusCode != test.statusCode {
+				t.Errorf("status code %d expected, got %d", test.statusCode, res.StatusCode)
+			}
+
+			if res.StatusCode != 500 && !strings.Contains(string(data), test.path) {
+				t.Errorf("%s does not contain %s", string(data), test.path)
 			}
 		})
 	}
