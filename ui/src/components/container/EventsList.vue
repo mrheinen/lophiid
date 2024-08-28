1b3bd09e288e48bb94a47c9862452d1c5afcfb42
niels <niels@prodsec.co>
2024-08-28 05:06:11 -0400

    Add subtype, update search documentation

--- ui/src/components/container/EventsList.vue
+++ ui/src/components/container/EventsList.vue
@@ -246,8 +246,8 @@ export default {
   },
   mounted() {
     if (this.$route.query.q) {
-      this.query = this.$route.query.q;
       this.$refs.searchBar.setQuery(this.$route.query.q);
+      this.query = this.$route.query.q;
     }
     this.loadEvents(true, function () {});
   },
