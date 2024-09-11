d509a52d2b11394af6f54b4f7120e1152e72987c
niels <niels@prodsec.co>
2024-09-11 07:27:42 -0400

    Add IsValidUUID method and tests

---
+++ pkg/util/general_test.go
@@ -0,0 +1,45 @@
+package util
+
+import "testing"
+
+func TestIsValidUUID(t *testing.T) {
+
+	for _, test := range []struct {
+		description string
+		uuid        string
+		valid       bool
+	}{
+		{
+			description: "UUID is valid",
+			uuid:        "a5c5f7c9-9b0f-4f6d-8a7d-3c6b9e0f6d8a",
+			valid:       true,
+		},
+		{
+			description: "UUID is valid - mixed case",
+			uuid:        "a5c5f7c9-9b0f-4f6d-8A7D-3c6b9e0f6d8a",
+			valid:       true,
+		},
+
+		{
+			description: "UUID is not corrupt",
+			uuid:        "a5c5f7c9-9b-4f6d-8a7d-3c6b9e0f6d8a",
+			valid:       false,
+		},
+		{
+			description: "UUID has invalid chars",
+			uuid:        "a5c5fXXX-9b-4f6d-8a7d-3c6b9e0f6d8a",
+			valid:       false,
+		},
+
+	} {
+
+		t.Run(test.description, func(t *testing.T) {
+			res := IsValidUUID(test.uuid)
+			if res != test.valid {
+				t.Errorf("got %v, want %v", res, test.valid)
+			}
+		})
+
+	}
+
+}
