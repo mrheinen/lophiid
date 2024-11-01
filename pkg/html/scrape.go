8404bc4d0d43c1e137e0f3f8d79d5b5a9860b829
niels <niels@prodsec.co>
2024-11-01 16:30:41 -0400

    Add test

--- pkg/html/scrape.go
+++ pkg/html/scrape.go
@@ -1,8 +1,25 @@
+// Lophiid distributed honeypot
+// Copyright (C) 2024 Niels Heinen
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 package html
 
 import (
 	"errors"
 	"fmt"
+	"log/slog"
 	"net/url"
 	"regexp"
 	"strings"
@@ -45,6 +62,8 @@ func ExtractResourceLinks(baseUrl string, htmlContent string) []string {
 							normalizedLink, err := normalizeLink(baseUrl, a.Val)
 							if err == nil {
 								resourceLinks[normalizedLink] = true
+							} else {
+								slog.Debug("skipping resource link", "error", err.Error())
 							}
 						}
 					}
--- pkg/html/scrape_test.go
+++ pkg/html/scrape_test.go
@@ -1,12 +1,59 @@
+// Lophiid distributed honeypot
+// Copyright (C) 2024 Niels Heinen
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 package html
 
 import (
+	"reflect"
 	"strings"
 	"testing"
 )
 
-func TestNormalizeLink(t *testing.T) {
+func TestExtractResourceLink(t *testing.T) {
+	for _, test := range []struct {
+		description  string
+		baseUrl      string
+		html         string
+		expectedUrls []string
+	}{
+		{
+			description:  "gets url",
+			baseUrl:      "http://example.org",
+			html:         "<html><script src=\"/foo.js\"></html>",
+			expectedUrls: []string{"http://example.org/foo.js"},
+		},
+		{
+			description:  "gets multiple urls",
+			baseUrl:      "http://example.org",
+			html:         "<html><script src=\"/foo.js\"></script><script src=\"http://example.org/aa\"></script></html>",
+			expectedUrls: []string{"http://example.org/foo.js", "http://example.org/aa"},
+		},
+	} {
 
+		t.Run(test.description, func(t *testing.T) {
+			urls := ExtractResourceLinks(test.baseUrl, test.html)
+
+			if !reflect.DeepEqual(urls, test.expectedUrls) {
+				t.Errorf("expected %+v, got %+v", test.expectedUrls, urls)
+			}
+		})
+	}
+}
+
+func TestNormalizeLink(t *testing.T) {
 	for _, test := range []struct {
 		description         string
 		inputUrl            string
