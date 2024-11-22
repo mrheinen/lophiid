3bf1e8f9dd879a4c9635eb210a187b8370dbf5c9
niels <niels@prodsec.co>
2024-11-22 14:19:15 -0500

    bump version

--- pkg/util/constants/shared_constants.go
+++ pkg/util/constants/shared_constants.go
@@ -2,7 +2,7 @@ package constants
 
 // The lophiid version. Needs to be bumped with every release and is used by
 // both the agent and the backend.
-const LophiidVersion = "0.11.1-alpha"
+const LophiidVersion = "0.20.0-alpha"
 
 // IP event sources are used to indicate the source of an IP event. The values
 // below need to be kept in sync with IP_EVENT_SOURCE in the database.
@@ -13,6 +13,7 @@ const (
 	IpEventSourceBackend  = "BACKEND"
 	IpEventSourceAnalysis = "ANALYSIS"
 	IpEventSourceWhois    = "WHOIS"
+	IpEventSourceAI       = "AI"
 )
 
 // This needs to be kept in sync with IP_EVENT_TYPE in the database.
