006886e9253692bad8b36fb7d0c5d9f8e2c33f77
niels <niels@prodsec.co>
2024-12-19 11:46:56 -0500

    Update the prompt

--- pkg/triage/describer/describer.go
+++ pkg/triage/describer/describer.go
@@ -56,11 +56,11 @@ type LLMResult struct {
 }
 
 const LLMSystemPrompt = `
-Analyze the provided HTTP response. Your response needs to be a raw JSON object response that is not formatted for displaying, without any text outside the JSON, that has the following keys:
+Analyze the provided HTTP request. Your response needs to be a raw JSON object that is not formatted for displaying, without any text outside the JSON, that has the following keys:
 
 description: One paragraph describing the intent of the request, if it is malicious and what application it targets. Describe the payload if the request is malicous. Do not include hostnames, IPs or ports.
-malicious: Use the string "yes" if the request is malicious. Else use the string "no".
-vulnerability_type: A string containing the Mitre CWE ID, starting with "CWE-" for weakness being exploited. Use an empty string if you don't know.
+malicious: Use the string "yes" if the request is malicious. Else "no".
+vulnerability_type: A string containing the Mitre CWE ID, starting with "CWE-" for the main weakness being exploited. Use an empty string if you don't know.
 application: A string with the targetted application/device name. An empty string if you don't know.
 cve: The relevant CVE if you know what vulnerability is exploited. An empty string if you don't know.
 
