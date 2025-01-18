527b99781c9da73ff7c8dde8a1f528f41715aed1
niels <niels@prodsec.co>
2025-01-18 09:29:49 -0500

    Add yara prepare command example

---
+++ scripts/yaraPrepareCommand-example.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# This is an example script that can be used to unpack the malware before the
+# yara scan runs. Unpacked files need to have an .unpacked extension.
+#
+# The script receives the file as the first argument.
+#
+## This script makes use of https://github.com/cloudflare/sandbox to sandbox the UPX command.
+
+MALWARE_FILE=$1
+
+LD_PRELOAD=/disk/niels/coding/sandbox/libsandbox.so SECCOMP_SYSCALL_ALLOW="access:arch_prctl:brk:chmod:chown:close:exit_group:fstat:futex:getrandom:ioctl:lseek:mmap:mprotect:munmap:newfstatat:openat:pread64:prlimit64:read:rename:rseq:rt_sigaction:set_robust_list:set_tid_address:sigaltstack:unlink:utimensat:write" upx -d ${MALWARE_FILE} -o ${MALWARE_FILE}.unpacked
