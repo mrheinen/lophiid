ae6f41b464eaec64bb4026302321f3fb061b8b78
niels <niels@prodsec.co>
2025-01-29 14:10:10 -0500

    updating the dependency packages

---
+++ pkg/database/queries.go
@@ -0,0 +1,28 @@
+// Lophiid distributed honeypot
+// Copyright (C) 2025 Niels Heinen
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+package database
+
+const QueryTotalRequestsPerMonthLastYear = `
+SELECT
+    TO_CHAR(DATE_TRUNC('month', created_at), 'YYYY-MM') AS month,
+    COUNT(*) as total_entries
+FROM request
+WHERE created_at >= NOW() - INTERVAL '12 months'
+GROUP BY month
+ORDER BY month DESC;
+
+`
