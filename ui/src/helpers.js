481f90095dcbbe28c7c96a2f77c9cf2181a60dcb
Niels <niels@prodsec.co>
2025-10-15 14:20:39 +0000

    make sure its a mm/dd/yyyy format

--- ui/src/helpers.js
+++ ui/src/helpers.js
@@ -101,9 +101,8 @@ export const sharedMixin = {
 export function getDateMinusMonths(monthsToSubtract) {
   const date = new Date();
   date.setMonth(date.getMonth() - monthsToSubtract);
-
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
   const year = date.getFullYear();
 
   return month + "/" + day + "/" + year;
