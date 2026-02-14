5b65e00199bf52f6b3459c4ad523756ef9fe1119
Niels <niels@prodsec.co>
2026-02-14 17:28:38 +0000

    Minor readability improvements

--- cmd/shell/main.go
+++ cmd/shell/main.go
@@ -63,13 +63,13 @@ func main() {
 
 	metricsRegistry := prometheus.NewRegistry()
 	llmMetrics := llm.CreateLLMMetrics(metricsRegistry)
-	responderLLMCfg, err := cfg.GetLLMConfig(cfg.AI.ShellEmulation.LLMConfig)
-	slog.Info("Using model", slog.String("model", responderLLMCfg.PrimaryLLM.Model))
+	shellLLMCfg, err := cfg.GetLLMConfig(cfg.AI.ShellEmulation.LLMConfig)
 	if err != nil {
 		slog.Error("error getting responder LLM config", slog.String("error", err.Error()))
 		return
 	}
-	primaryManager := llm.GetLLMManager(responderLLMCfg, llmMetrics)
+	slog.Info("Using model", slog.String("model", shellLLMCfg.PrimaryLLM.Model))
+	primaryManager := llm.GetLLMManager(shellLLMCfg, llmMetrics)
 
 	shc := shell.NewShellClient(primaryManager, dbc)
 
